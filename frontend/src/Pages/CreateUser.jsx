93dff445f8682135f7ac33a6cddfc61ca4af8c8c
PrincyChauhan <chauhanprincee7@gmail.com>
2025-03-19 16:54:26 +0530

    Create User API

--- frontend/src/Pages/CreateUser.jsx
+++ frontend/src/Pages/CreateUser.jsx
@@ -1,97 +1,145 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Spin } from "antd";
 const CreateUser = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    role: "",
+  });
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setError("You need to login first");
+      return;
+    }
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/auth/create-invite-user",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      setSuccess(response.data.message);
+      setError("");
+      setFormData({ username: "", email: "", password: "", role: "" }); // Clear form
+      setTimeout(() => {
+        navigate("/dashboard");
+      }, 100);
+    } catch (error) {
+      console.error("Error while creating user:", error);
+      setError("Failed to create user.");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
-      <div>
-        <Navbar />
-      </div>
+      <Navbar />
       <div className="flex flex-row">
         <Sidebar />
-        <div className="flex flex-col items-center  p-10 w-full h-[100%] font-poppins">
-          <div className="bg-white p-8 rounded-lg shadow-lg w-[50%] h-[50%]">
+        <div className="flex flex-col items-center p-10 w-full h-full font-poppins">
+          <div className="bg-white p-8 rounded-lg shadow-lg w-1/2 h-1/2">
             <h2 className="text-3xl font-bold text-center mb-8 text-[#0C0950]">
               Create User
             </h2>
+            {error && <p className="text-red-500">{error}</p>}
+            {success && <p className="text-green-500">{success}</p>}
 
-            <form className="w-full h-full">
-              <div className="mb-4">
-                <label
-                  htmlFor="username"
-                  className="block text-sm font-medium text-gray-700 mb-2"
-                >
-                  User Name
-                </label>
-
-                <input
-                  type="text"
-                  id="username"
-                  name="username"
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md"
-                  required
-                />
+            {loading ? (
+              <div className="flex justify-center items-center h-full">
+                <Spin size="large" />
               </div>
+            ) : (
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label className="block text-sm font-medium text-gray-700 mb-2">
+                    User Name
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                    required
+                  />
+                </div>
 
-              <div className="mb-4">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700 mb-2"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md"
-                  required
-                />
-              </div>
+                <div className="mb-4">
+                  <label className="block text-sm font-medium text-gray-700 mb-2">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                    required
+                  />
+                </div>
 
-              <div className="mb-4">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700 mb-2"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md"
-                  required
-                />
-              </div>
+                <div className="mb-4">
+                  <label className="block text-sm font-medium text-gray-700 mb-2">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                    required
+                  />
+                </div>
 
-              <div className="mb-4">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700 mb-2"
-                >
-                  Role
-                </label>
-                <select
-                  id="role"
-                  name="role"
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md"
-                  required
-                >
-                  <option value="user" className="bg-[#0C0950]">
-                    User
-                  </option>
-                </select>
-              </div>
+                <div className="mb-4">
+                  <label className="block text-sm font-medium text-gray-700 mb-2">
+                    Role
+                  </label>
+                  <select
+                    name="role"
+                    value={formData.role}
+                    onChange={handleChange}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                    required
+                  >
+                    <option value="">Select Role</option>
+                    <option value="user">User</option>
+                  </select>
+                </div>
 
-              <button
-                type="submit"
-                className="w-full py-2 px-4 bg-[#0C0950] text-white font-semibold rounded-lg hover:bg-[#161179] "
-              >
-                Create User
-              </button>
-            </form>
+                <button
+                  type="submit"
+                  className="w-full py-2 px-4 bg-[#0C0950] text-white font-semibold rounded-lg hover:bg-[#161179]"
+                >
+                  Create User
+                </button>
+              </form>
+            )}
           </div>
         </div>
       </div>
